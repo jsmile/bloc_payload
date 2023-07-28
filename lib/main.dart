ae9c36ad801f9e767c5afb011a167fefc10afd73
Byeong Gui Jeong <jsmile@naver.com>
2023-07-28 19:20:50 +0900

    bloc_payload basic modification

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'BLoC Payload'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text(
+            'Change Theme',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          onPressed: () {
+            final randInt = Random().nextInt(10);
+            print('*** randInt : $randInt.');
+          },
+        ),
+      ),
+    );
+  }
+}
